511eaac3962af329ee76afc792fa773cd56ba87c
stanleycyang <stanley@stanleycyang.com>
2016-01-15 23:36:56 -0800

    Changed the beginning text

--- client/container/index.js
+++ client/container/index.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 export default class App extends Component {
   render() {
     return (
-      <div>Hello world</div>
+      <div>Hello world! Love, Stanley</div>
     )
   }
 }
