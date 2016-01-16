9ce4495401e7a599d1b2d6cc5f880384703b3727
stanleycyang <stanley@stanleycyang.com>
2016-01-15 23:20:09 -0800

    Added nodemon

--- client/container/index.js
+++ client/container/index.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 export default class App extends Component {
   render() {
     return (
-      <div>Hey world</div>
+      <div>Hello world</div>
     )
   }
 }
